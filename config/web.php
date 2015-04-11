98a811d49993a7796edd65f1fbb1ce547cd713c1
Serhii Prostakov <s.prostakov@yandex.com>
2015-04-11 23:32:11 +0300

    still

--- config/web.php
+++ config/web.php
@@ -60,7 +60,7 @@ $config = [
 //                'quiz/<quiz_id:\d+>/<chapter_num:\d+>' => 'chapter/view',
 //                'quiz/<quiz_id:\d+>/<chapter_num:\d+>/edit' => 'chapter/update',
 //                'quiz/<quiz_id:\d+>/<chapter_num:\d+>/delete' => 'chapter/delete',
-            ]
+//            ]
         ],
     ],
     'params' => $params,
