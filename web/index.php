0a8dbaf15830efac87a2c002f8f3bd3345a7e4c1
Serhii Prostakov <s.prostakov@yandex.com>
2015-04-11 18:28:41 +0300

    still

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <?php
 
 // comment out the following two lines when deployed to production
-if (strlen(getenv('YII_ENV')) == 0) define('YII_ENV', getenv('YII_ENV'));
-if (strlen(getenv('YII_DEBUG')) == 0) define('YII_DEBUG', getenv('YII_DEBUG'));
-
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+//if (strlen(getenv('YII_ENV')) == 0) define('YII_ENV', getenv('YII_ENV'));
+//if (strlen(getenv('YII_DEBUG')) == 0) define('YII_DEBUG', getenv('YII_DEBUG'));
+//
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
