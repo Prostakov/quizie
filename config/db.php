2ccaaa681409552520d8467c83050908e3682875
Serhii Prostakov <s.prostakov@yandex.com>
2015-04-11 15:20:53 +0300

    established local connection to db, now time for heroku

--- config/db.php
+++ config/db.php
@@ -1,9 +1,17 @@
 <?php
 
 return [
+    // Local:
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yii2_quizie',
     'username' => 'root',
-    'password' => '',
+    'password' => '1234',
     'charset' => 'utf8',
+
+    // Production on heroku:
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'pgsql:host=localhost;port=5432;dbname=mydatabase',
+//    'username' => 'root',
+//    'password' => '',
+//    'charset' => 'utf8',
 ];
